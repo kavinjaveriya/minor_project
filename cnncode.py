b33a6539e5434348435cd7a8f7dcb5a6df895d97
kavinjaveriya <kavinjaveriya@gmail.com>
2021-01-09 23:36:06 +0530

    path change

--- cnncode.py
+++ cnncode.py
@@ -24,8 +24,8 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accur
 from keras_preprocessing.image import ImageDataGenerator
 traingen = ImageDataGenerator(rescale=1./255 , zoom_range=0.2 , horizontal_flip=True ,  vertical_flip=True )
 testgen = ImageDataGenerator(rescale=1./255)
-trainset = traingen.flow_from_directory('/cnn_code/dataset/train/' , target_size=(64,64) , batch_size=32 , class_mode='categorical' )
-testset = testgen.flow_from_directory('/cnn_code/dataset/val/' , target_size=(64,64) , batch_size=32 , class_mode='categorical' )
+trainset = traingen.flow_from_directory('/code/dataset/train/' , target_size=(64,64) , batch_size=32 , class_mode='categorical' )
+testset = testgen.flow_from_directory('/code/dataset/val/' , target_size=(64,64) , batch_size=32 , class_mode='categorical' )
 model.fit(trainset , epochs=5 , validation_data=testset , steps_per_epoch=70, validation_steps=10  )
 model.save("cnn.h5")
 scores = model.evaluate(testset,verbose=1)
